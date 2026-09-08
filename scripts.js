94b3549d8458adb4c988365d32c29c22749cc82f
gab <gabbdc128@gmail.com>
2026-09-09 03:07:24 +0800

    Change the logic of the emeyl and Add notification in github logo
    Made javascript as the one who's in charge on what the text will be inside of >emeyl-text< span and Add a notification or drop-down-notif? under the github logo for a bit detail to it

--- scripts.js
+++ scripts.js
@@ -1,9 +1,38 @@
-const emailText = document.querySelector(".emeyl-text");
-const emailDefault = emailText.textContent;
+const emeylContain = document.querySelector(".emeyl-text");
+const email = document.querySelector(".email");
+const fullEmail = document.createElement("span");
+fullEmail.className = "full-email";
+fullEmail.textContent = "adsasdasd@gmail.com";
 
-emailText.addEventListener("mouseenter", () => {
-  emailText.textContent = "adsasdasd@gmail.com";
+function defaultEmail() {
+  const defaultEmail = document.createElement("span");
+  defaultEmail.className = "default-email";
+  defaultEmail.textContent = "Email";
+
+  emeylContain.prepend(defaultEmail);
+}
+defaultEmail();
+email.addEventListener("mouseenter", () => {
+  const emailTemp = document.querySelector(".default-email");
+  emeylContain.appendChild(fullEmail);
+  emailTemp.remove();
 });
-emailText.addEventListener("mouseleave", () => {
-  emailText.textContent = emailDefault;
+
+email.addEventListener("mouseleave", () => {
+  const emailTemp = document.querySelector(".full-email");
+  emailTemp.remove();
+  defaultEmail();
+});
+
+const githubLogo = document.querySelector(".github-logo");
+const githubInfo = document.createElement("span");
+githubInfo.className = "github-info";
+githubInfo.textContent = "View my GitHub Profile";
+
+githubLogo.addEventListener("mouseenter", () => {
+  githubLogo.appendChild(githubInfo);
+});
+
+githubLogo.addEventListener("mouseleave", () => {
+  githubInfo.remove();
 });
